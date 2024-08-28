dca29641d8610b19ccf2d10cbf7ff6d85deb58e7
Terraformer9x <83795041+Terraformer9x@users.noreply.github.com>
2024-08-28 02:49:31 -0400

    Initial commit
    Update manifest.json

---
+++ JesterDoorSlam/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace JesterDoorSlam;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private readonly Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+    public static Plugin Instance;
+    
+    private void Awake()
+    {
+        Instance ??= this;
+
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        harmony.PatchAll(typeof(JesterPatch));
+    }
+}
+
+[HarmonyPatch(typeof(JesterAI))]
+public class JesterPatch
+{
+    [HarmonyPatch(nameof(JesterAI.Update))]
+    [HarmonyPostfix]
+    private static void SlamOpenDoors(ref JesterAI __instance)
+    {
+        if (__instance.currentBehaviourStateIndex == 2 && __instance.agent.speed >= 10f)
+        {
+            __instance.useSecondaryAudiosOnAnimatedObjects = true;
+        }
+        else
+        {
+            __instance.useSecondaryAudiosOnAnimatedObjects = false;
+        }
+    }
+}
